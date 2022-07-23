24a42da1d23d2767d22778cfb4c006888ddf7289
Ali Villegas <alibryan.villegas@gmail.com>
2022-07-22 19:33:04 -0500

    added site stat tracking to know  num of visits

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 const localStorageMissionsName = "missionsData";
+let estimatedVisitorsUsingTheTool = 0;
+
 function strikeSideMission(sideMissionData) {
   const missionId = sideMissionData?.getAttribute("for");
   const checked = sideMissionData?.checked || false;
@@ -18,6 +20,20 @@ function strikeSideMission(sideMissionData) {
       JSON.stringify(missionsInLocalStorage)
     );
   } else {
+    var xhr = new XMLHttpRequest();
+    xhr.open(
+      "GET",
+      "https://api.countapi.xyz/hit/localhost/personUsingMoonMeisterTracker"
+    );
+    xhr.responseType = "json";
+    xhr.onload = function () {
+      console.log(`This button has been clicked ${this.response.value} times!`);
+      estimatedVisitorsUsingTheTool = this?.response?.value;
+      document.getElementById(
+        "estimatedVisitorsUsingTheTool"
+      ).innerHTML = `Estimated number of visitors using the tool: ${this.response.value}`;
+    };
+    xhr.send();
     let missionsInLocalStorage = {};
     missionsInLocalStorage[missionId] = checked;
     localStorage.setItem(
@@ -28,6 +44,16 @@ function strikeSideMission(sideMissionData) {
 }
 
 function loadMissions() {
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://api.countapi.xyz/hit/localhost/personVisitingMoonMeisterTracker"
+  );
+  xhr.responseType = "json";
+  xhr.onload = function () {
+    console.log(`Estimated number of visitors ${this.response.value}`);
+  };
+  xhr.send();
   let missionsInLocalStorage = localStorage?.getItem(localStorageMissionsName);
   if (missionsInLocalStorage) {
     missionsInLocalStorage = JSON.parse(missionsInLocalStorage);
